ee00e119c3692e717afb5082c30b7f79092d6d8e
Rizwan Shivalli <rizwan.a@simplificommerce.com>
2021-04-06 20:06:35 +0530

    login fail message

--- src/models/login.ts
+++ src/models/login.ts
@@ -6,9 +6,10 @@ import { message } from 'antd';
 import { removeToken, setToken } from '@/utils/token';
 
 export type StateType = {
-  status?: 'ok' | 'error';
+  status?: 'ok' | 'error' | number;
   type?: string;
   currentAuthority?: 'user' | 'guest' | 'admin';
+  message?: string;
 };
 
 export type LoginModelType = {
@@ -41,8 +42,8 @@ const Model: LoginModelType = {
       if (response.statusCode === 200) {
         message.success('🎉 🎉 🎉  login successful');
         window.location.href = '/';
-      } else if (response.status === 400) {
-        message.error('Email or password incorrect');
+      } else if (response.statusCode === 400) {
+        message.error(response.message);
       }
       return response;
     },
@@ -60,11 +61,12 @@ const Model: LoginModelType = {
 
   reducers: {
     changeLoginStatus(state, { payload }) {
-      setAuthority(payload.currentAuthority);
-      setToken(payload?.data.jwt);
+      setAuthority(payload?.currentAuthority);
+      setToken(payload?.data?.jwt);
       return {
         ...state,
-        status: payload.status,
+        status: payload?.statusCode,
+        message: payload.message ? payload.message : 'Login Successfull',
         type: payload.type,
       };
     },
