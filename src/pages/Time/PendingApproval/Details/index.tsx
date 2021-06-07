b4b7ed7d1c6c5988dd3a9e327cc07aa5c8b3b9c0
unknown <pramod.h@simplificommerce.com>
2021-06-07 12:12:54 +0530

    pendding approvel page updated check

--- src/pages/Time/PendingApproval/Details/index.tsx
+++ src/pages/Time/PendingApproval/Details/index.tsx
@@ -1,10 +1,11 @@
 import { ProGridContainer, ProIntlProvider } from '@/common';
-import { Button, Col, message, Row } from 'antd';
+import { Button, Col, message, Row, Progress } from 'antd';
 import React, { useEffect, useRef, useState } from 'react';
 import { getPendingApprovalByID, approveTimesheet } from '../../service';
 import { humanize, createTableColumns } from '@/utils/generalUtils';
 import ProTable, { ActionType } from '@ant-design/pro-table';
 import { CheckOutlined, MailOutlined } from '@ant-design/icons';
+import './index.less';
 
 const ApprovalDetails = (props: any) => {
   const [id] = useState<string>(props?.match?.params?.id);
@@ -52,14 +53,50 @@ const ApprovalDetails = (props: any) => {
   return (
     <ProGridContainer>
       <Row>
-        <Col span={24}>
-          <div>Hello World</div>
+        <Col span={24} className="heading">
+          <div className="head_title">
+            <h1>Approve [SAMPLE] Kiran’s Timesheet for 24 – 30 May 2021</h1>
+            <p>[SAMPLE] Client A (Fixed Fee Project)</p>
+          </div>
+        </Col>
+        <Col span={6}>
+          <div className="paragraphs">
+            <p>Hours Tracked</p>
+            <h2>6:40</h2>
+          </div>
+        </Col>
+        <Col span={4} className="progress_bar">
+          <Progress type="circle" percent={99} width={80} />
+        </Col>
+        <Col span={8} className="Circle_Wraps">
+          <div className="left_text">
+            <p className="Billable_Hourse">Billable Hours</p>
+          </div>
+          <div className="left_text">
+            <p className="green_box"></p>
+            <p>
+              <strong> 6:40 Billable</strong>
+            </p>
+          </div>
+          <div className="left_text">
+            <p className="skyblue_box"></p>
+            <p>
+              <strong> 0:00 Non-Billable</strong>
+            </p>
+          </div>
+        </Col>
+        <Col span={6}>
+          <div className="paragraphs">
+            <p>Expenses</p>
+            <h2>$0.00</h2>
+          </div>
         </Col>
       </Row>
       <Row>
         <Col span={24}>
           {' '}
           <ProIntlProvider>
+            <h2>Pending Hours</h2>
             <ProTable
               loading={loading}
               actionRef={actionRef}
@@ -73,6 +110,7 @@ const ApprovalDetails = (props: any) => {
         </Col>
         <Col span={24}>
           <Button
+            className="Approvel_button"
             size="large"
             key="2"
             type="primary"
@@ -92,7 +130,14 @@ const ApprovalDetails = (props: any) => {
             {data?.status !== 'Approved' ? `Approve Timesheet` : `Approved Timesheet`}
           </Button>
           ,
-          <Button size="large" key="3" type="primary" onClick={() => {}} disabled={loading}>
+          <Button
+            className="Approvel_button"
+            size="large"
+            key="3"
+            type="primary"
+            onClick={() => {}}
+            disabled={loading}
+          >
             <MailOutlined />
             Email {data?.user_name}
           </Button>
