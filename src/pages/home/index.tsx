a3d4300eae4f0637b24a085b7fa8344990d8ab48
unknown <pramod.h@simplificommerce.com>
2021-06-22 11:48:00 +0530

    changes made

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,22 +1,20 @@
 import { ProDivider } from '@/common';
 import {
-  CloseOutlined,
   CheckCircleOutlined,
   PlusCircleOutlined,
   ClockCircleOutlined,
-  ExclamationCircleOutlined,
+  PlayCircleOutlined,
 } from '@ant-design/icons';
-import { Col, Row } from 'antd';
+import { Col, Row, Checkbox } from 'antd';
 import React from 'react';
 import './index.less';
 
 const Home = () => {
   return (
     <Row>
-      <Col span={12}>
+      <Col span={24}>
         <p className="Home_Welcome_Wraps">Welcome to Simplifi Team!</p>
       </Col>
-      <Col span={12}></Col>
       <ProDivider></ProDivider>
       <Col span={18}>
         <div>
@@ -26,31 +24,31 @@ const Home = () => {
           <span className="Home_icons_Wraps">
             <CheckCircleOutlined />
           </span>
-          <span className="Home_fonts_wraps">Learn the basics of time tracking</span>
+          <p className="Home_fonts_wraps">Learn the basics of time tracking</p>
         </div>
         <div className="Home_clicking_divs_wraps">
           <span className="Home_icons_Wraps">
             <PlusCircleOutlined />
           </span>
-          <span className="Home_fonts_wraps">Track your first hour</span>
+          <p className="Home_fonts_wraps">Track your first hour</p>
         </div>
         <div className="Home_clicking_divs_wraps">
           <span className="Home_icons_Wraps">
             <ClockCircleOutlined />
           </span>
-          <span className="Home_fonts_wraps">Peruse your projects</span>
+          <p className="Home_fonts_wraps">Peruse your projects</p>
         </div>
         <div className="Home_clicking_divs_wraps">
           <span className="Home_icons_Wraps">
-            <ExclamationCircleOutlined />
+            <PlayCircleOutlined />
           </span>
-          <span className="Home_fonts_wraps">Get the desktop and mobile apps</span>
+          <p className="Home_fonts_wraps">Get the desktop and mobile apps</p>
         </div>
       </Col>
       <Col span={6}></Col>
       <Col span={18}>
         <div className="belowlinks_wraps">
-          <p>
+          <p className="para_links">
             <a href="">Webinars</a>
           </p>
           <p>
@@ -58,22 +56,27 @@ const Home = () => {
           </p>
         </div>
         <div className="belowlinks_wraps">
-          <p>
+          <p className="para_links">
             <a href="">Help Center</a>
           </p>
-          <p>Our knowledge base and FAQs.</p>
+          <p>
+            Our knowledge base<br></br> and FAQs.
+          </p>
         </div>
         <div className="belowlinks_wraps">
-          <p>
+          <p className="para_links">
             <a href="">Support</a>
           </p>
-          <p>Friendly experts, always ready to help.</p>
+          <p>
+            Friendly experts, always <br></br>ready to help.
+          </p>
         </div>
       </Col>
       <ProDivider></ProDivider>
       <Col span={24}>
-        <input type="checkbox"></input>
-        <span>Email me occasional updates, tips, and interesting stories</span>
+        <Checkbox>
+          <span> Email me occasional updates, tips, and interesting stories</span>
+        </Checkbox>
       </Col>
     </Row>
   );
